e737a3b684eeb2a663cb76223809b75fae7f5bac
wb263970 <wb-fjk263970@alibaba-inc.com>
2017-07-14 15:02:49 +0800

    注解类示例

--- springmvc/src/main/java/com/xiaokang/king/first/DemoPathVariable.java
+++ springmvc/src/main/java/com/xiaokang/king/first/DemoPathVariable.java
@@ -1,10 +1,7 @@
 package com.xiaokang.king.first;
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Arrays;
 import java.util.Map;
 
