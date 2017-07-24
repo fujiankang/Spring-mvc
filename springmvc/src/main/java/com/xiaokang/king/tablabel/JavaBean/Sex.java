329d41f57c95dfd256d49c17672d13c394024866
wb263970 <wb-fjk263970@alibaba-inc.com>
2017-07-24 17:57:47 +0800

    radiobutton

---
+++ springmvc/src/main/java/com/xiaokang/king/tablabel/JavaBean/Sex.java
@@ -0,0 +1,16 @@
+package com.xiaokang.king.tablabel.JavaBean;
+
+/**
+ * Created by wb263970 on 2017/7/24.
+ */
+public class Sex {
+    private  String sex;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
